b93d25e6f93b7f26ac02e349bc578ed055809aee
AndriantsehenoAnja <andriantsehenoanja926@gmail.com>
2026-06-16 11:10:56 +0300

    Enhance DispatcherServlet to forward requests for static resources

--- core/DispatcherServlet.java
+++ core/DispatcherServlet.java
@@ -17,12 +17,46 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                    String uri = request.getRequestURI();
+
+        if (uri.endsWith(".html")
+                || uri.endsWith(".css")
+                || uri.endsWith(".js")
+                || uri.endsWith(".png")
+                || uri.endsWith(".jpg")
+                || uri.endsWith(".gif")
+                || uri.endsWith(".ico")
+                || uri.endsWith(".svg")
+                || uri.endsWith(".jsp")) {
+
+            request.getServletContext()
+                .getNamedDispatcher("default")
+                .forward(request, response);
+            return;
+        }
         affichage(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+                    String uri = request.getRequestURI();
+
+        if (uri.endsWith(".html")
+                || uri.endsWith(".css")
+                || uri.endsWith(".js")
+                || uri.endsWith(".png")
+                || uri.endsWith(".jpg")
+                || uri.endsWith(".gif")
+                || uri.endsWith(".ico")
+                || uri.endsWith(".svg")
+                || uri.endsWith(".jsp")) {
+
+            request.getServletContext()
+                .getNamedDispatcher("default")
+                .forward(request, response);
+            return;
+        }
         affichage(request, response);
 
     }
