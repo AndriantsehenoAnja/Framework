daa25d206102b61e7bb05297c171c43f5eeb07cf
AndriantsehenoAnja <andriantsehenoanja926@gmail.com>
2026-06-19 15:17:25 +0300

    Implement UrlMapping annotation and enhance ControllerUtils for dynamic method discovery

--- utils/ControllerUtils.java
+++ utils/ControllerUtils.java
@@ -1,31 +1,61 @@
 package utils;
+
 import java.io.File;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.reflect.Method;
+import mg.etu4370.annotation.UrlMapping;
+import utils.Dto.InfoMethodeAndController;
 public class ControllerUtils {
-    public boolean isAnnotationMethod(Class<?> clazz){
-        if(clazz.isAnnotationPresent(mg.etu4370.annotation.UrlMapping.class)){
+
+    public static boolean isAnnotationMethod(Method m) {
+        if (m.isAnnotationPresent(UrlMapping.class)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
-    public List<Method> findMethod(Class<?> clazz){
+
+    public static List<Method> findMethod(Class<?> clazz) {
         List<Method> methods = new ArrayList<>();
-        for(Method method : clazz.getDeclaredMethods()){
-            if(isAnnotationMethod(method.getClass())){
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (isAnnotationMethod(method)) {
                 methods.add(method);
             }
         }
         return methods;
     }
+    public static List<InfoMethodeAndController> findAllMethodes(String packageName){
+        
+        List<InfoMethodeAndController> infoMethodeAndControllers = new ArrayList<>();
+        List<Class<?>> controllerClasses = getControllers(packageName);
+        for (Class<?> controllerClass : controllerClasses) {
+            List<Method> methods = findMethod(controllerClass);
+            for (Method method : methods) {
+                UrlMapping urlMapping = method.getAnnotation(UrlMapping.class);
+                String url = urlMapping.value();
+                String methodName = method.getName();
+                String controllerName = controllerClass.getSimpleName();
+                InfoMethodeAndController infoMethodeAndController = new InfoMethodeAndController(url, methodName, controllerName);
+                infoMethodeAndControllers.add(infoMethodeAndController);
+            }
+        }
+        return infoMethodeAndControllers;
+    }
+    public static InfoMethodeAndController findClassByUrl(List<InfoMethodeAndController> infoMethodeAndControllers, String url) {
+        for (InfoMethodeAndController infoMethodeAndController : infoMethodeAndControllers) {
+            if(infoMethodeAndController.getUrl().equals(url)){
+                return infoMethodeAndController;
+            }
+        }
+        return null;
+    }
 
+    // getContextPath
     public static List<Class<?>> getControllers(String packageName) {
         List<Class<?>> classe = new ArrayList<>();
 
-        List<Class<?>> classes = findClass(packageName,classe);
+        List<Class<?>> classes = findClass(packageName, classe);
         List<Class<?>> controllerNames = new ArrayList<>();
         for (Class<?> clazz : classes) {
             if (clazz.isAnnotationPresent(mg.etu4370.annotation.Controller.class)) {
@@ -34,7 +64,8 @@ public class ControllerUtils {
         }
         return controllerNames;
     }
-    public static List<Class<?>> findClass(String packageName,List<Class<?>> classes){
+
+    public static List<Class<?>> findClass(String packageName, List<Class<?>> classes) {
         String packagePath = packageName.replace('.', '/');
         try {
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -50,10 +81,9 @@ public class ControllerUtils {
                         if (file.getName().endsWith(".class")) {
                             String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
                             classes.add(Class.forName(className));
-                        }
-                        else if(file.isDirectory()){
+                        } else if (file.isDirectory()) {
                             // classes.add(Class.forName(file.getName()));
-                            findClass(packageName + "." + file.getName(),classes);
+                            findClass(packageName + "." + file.getName(), classes);
                         }
                     }
                 } else {
